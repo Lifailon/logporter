b08281a8e616ffee66879c6c42095a473feccf73
Lifailon <116945542+Lifailon@users.noreply.github.com>
2026-08-22 14:45:19 +0300

    add background worker for image update check and volume metrics moved to package

--- internal/metrics/images.go
+++ internal/metrics/images.go
@@ -2,18 +2,63 @@ package metrics
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 	"strings"
 	"sync"
+	"time"
 
+	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/client"
 )
+
+type imageMetric struct {
+	image string
+	tag   string
+	sha   string
+	size  int
+}
 
 type imageUpdateMetrics struct {
 	tag           string
 	currentDigest string
 	remoteDigest  string
 	update        int
+}
+
+func (m *Metrics) getImagesMetrics(dockerClient *client.Client) ([]imageMetric, error) {
+	var imageMetrics []imageMetric
+	imageOptions := image.ListOptions{SharedSize: true}
+	images, err := dockerClient.ImageList(context.Background(), imageOptions)
+	if err != nil {
+		return nil, fmt.Errorf("error getting image list: %v", err)
+	}
+	for _, image := range images {
+		tag := "none"
+		if len(image.RepoTags) > 0 {
+			tag = image.RepoTags[0]
+		}
+		repoDigest := image.ID
+		if len(image.RepoDigests) > 0 {
+			repoDigest = image.RepoDigests[0]
+		}
+		shaIndex := strings.Index(repoDigest, "sha256:")
+		if shaIndex != -1 {
+			repoDigest = repoDigest[shaIndex+7:]
+		}
+		size := int(image.Size)
+		sharedSize := int(image.SharedSize)
+		if sharedSize > 0 {
+			size = size - sharedSize
+		}
+		data := imageMetric{
+			tag:  tag,
+			sha:  repoDigest,
+			size: size,
+		}
+		imageMetrics = append(imageMetrics, data)
+	}
+	return imageMetrics, nil
 }
 
 func (m *Metrics) checkImageUpdate(dockerClient *client.Client, image imageMetric) (imageUpdateMetrics, error) {
@@ -43,63 +88,47 @@ func (m *Metrics) checkImageUpdate(dockerClient *client.Client, image imageMetri
 	return imageStatus, nil
 }
 
-func (m *Metrics) getImageUpdateMetrics(dockerClient *client.Client, hostname string, logger *slog.Logger) []imageUpdateMetrics {
+func (m *Metrics) getImagesUpdateMetrics(dockerClient *client.Client, logger *slog.Logger) []imageUpdateMetrics {
 	if len(m.imageMetrics) > 0 {
-		// metrics := make([]imageUpdateMetrics, 0, len(m.imageMetrics))
-		// var wg sync.WaitGroup
-		// var mu sync.Mutex
-		// wg.Add(len(m.imageMetrics))
-		// for _, image := range m.imageMetrics {
-		// 	go func(image imageMetric) {
-		// 		defer wg.Done()
-		// 		status, err := m.checkImageUpdate(dockerClient, image)
-		// 		if err != nil {
-		// 			logger.Error("failed to inspect image distribution", "image", image.tag, "error", err)
-		// 		} else {
-		// 			mu.Lock()
-		// 			metrics = append(metrics, status)
-		// 			mu.Unlock()
-		// 		}
-		// 	}(image)
-		// }
-		// wg.Wait()
-		// return metrics
-
-		// Создаем буферизированный канал для сбора результатов работы горутин
-		ch := make(chan imageUpdateMetrics, len(m.imageMetrics))
-		// Объявляем группу ожиданеия для отслеживания жизненного цикла всех запущенных горутин
+		metrics := make([]imageUpdateMetrics, 0, len(m.imageMetrics))
 		var wg sync.WaitGroup
-		// Заполняем счетчик (выделяем память заранее)
+		var mu sync.Mutex
 		wg.Add(len(m.imageMetrics))
-		// Создаем буферизированный канал (семафор) для ограничения одновременно выполняющихся задач
-		// semaphore := make(chan struct{}, 10)
 		for _, image := range m.imageMetrics {
 			go func(image imageMetric) {
-				// Гарантируем (даже в случае паники) уменьшение счетчика WaitGroup на 1 при выходе из горутины
 				defer wg.Done()
-				// Пытаемся отправить пустую структуру (0 байт) в семафор
-				// Eсли в канале уже есть 10 элементов, горутина засыпает и ждет своей очереди
-				// semaphore <- struct{}{}
 				status, err := m.checkImageUpdate(dockerClient, image)
-				// Извлекаем элемент из семафора и выбрасываем его
-				// <-semaphore
 				if err != nil {
 					logger.Error("failed to inspect image distribution", "image", image.tag, "error", err)
 				} else {
-					ch <- status
+					mu.Lock()
+					metrics = append(metrics, status)
+					mu.Unlock()
 				}
 			}(image)
 		}
-		// Блокируем основной поток и ждем, пока все запущенные горутины вызовут wg.Done()
 		wg.Wait()
-		// Закрываем канал, чтобы цикл for-range понял, что все данные получены
-		close(ch)
-		metrics := make([]imageUpdateMetrics, 0, len(ch))
-		for status := range ch {
-			metrics = append(metrics, status)
-		}
 		return metrics
 	} else {
 		return nil
 	}
+}
+
+func (m *Metrics) ImageMetricsWorker(dockerClient *client.Client, logger *slog.Logger) {
+	start := time.Now()
+	if len(m.imageMetrics) == 0 {
+		var err error
+		m.imageMetrics, err = m.getImagesMetrics(dockerClient)
+		if err != nil {
+			logger.Error("failed to get image metrics", "error", err)
+		}
+	}
+	m.imageUpdateMetrics = m.getImagesUpdateMetrics(dockerClient, logger)
+	imageCount := len(m.volumeMetrics)
+	logger.Info(
+		"collecting image update metrics",
+		"source", "background worker",
+		"images", imageCount,
+		"duration", time.Since(start).Round(time.Millisecond),
+	)
 }
